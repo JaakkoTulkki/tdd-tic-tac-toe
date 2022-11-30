467e15b72834d8354658a77ea27831ecc82b8abb
Jaakko Tulkki <jaakko.tulkki.npm@gmail.com>
2022-11-30 17:41:51 +0000

    on step 2 before calculating winner

--- src/tic-tac-toe/game.test.ts
+++ src/tic-tac-toe/game.test.ts
@@ -1,5 +1,87 @@
+import {Cell, TicTacToe} from "./game";
+
+
 describe('TicTacToe', function () {
-    it('should work', function () {
+    let game: TicTacToe;
+    let board: Cell[][];
+
+    beforeEach(() => {
+        game = new TicTacToe();
+        board = game.getBoard();
+    })
+
+    it('should initialise', function () {
         expect(new TicTacToe()).toBeDefined();
     });
+
+    it('should create a board', function () {
+        expect(board.length).toEqual(3);
+    });
+
+    it('should create a board of 9 cells', function () {
+        expect(board.flat().length).toEqual(9);
+    });
+
+    it('should a board of objects telling the cells state', function () {
+        expect(board).toEqual([
+            [{occupier: undefined, coordinates: '0-0'}, {occupier: undefined, coordinates: '0-1'}, {occupier: undefined, coordinates: '0-2'}],
+            [{occupier: undefined, coordinates: '1-0'}, {occupier: undefined, coordinates: '1-1'}, {occupier: undefined, coordinates: '1-2'}],
+            [{occupier: undefined, coordinates: '2-0'}, {occupier: undefined, coordinates: '2-1'}, {occupier: undefined, coordinates: '2-2'}],
+        ]);
+    });
+
+    it('should create a board of 9 empty cells', function () {
+        expect(board.flat().filter(c => typeof c.occupier === 'undefined').length).toEqual(9);
+    });
+
+    it('should let the user click on empty cells', function () {
+        const mid = board[1][1];
+        const {newBoard} = game.tick('X', mid.coordinates);
+        expect(newBoard[1][1].occupier).toEqual('X');
+        expect(newBoard[1][1].coordinates).toEqual('1-1');
+    });
+    it('should not let the X user tick twice in a row', function () {
+        game.tick('X', '1-1');
+
+        expect(() => game.tick('X', '1-1')).toThrow("It's Y's turn.");
+    });
+
+    it('should not let the Y user tick twice in a row', function () {
+        game.tick('Y', '1-1');
+
+        expect(() => game.tick('Y', '1-1')).toThrow("It's X's turn.");
+    });
+    it('should let know whose turn it is', function () {
+        let {nextToPlay} = game.tick('X', '1-1');
+        expect(nextToPlay).toEqual('Y');
+        let {nextToPlay: xPlayer} = game.tick('Y', '1-2');
+        expect(xPlayer).toEqual('X');
+    });
+
+    it('should not let the a cell to be clicked twice', function () {
+        game.tick('X', '1-1');
+        expect(() => game.tick('Y', '1-1')).toThrow('The cell is already occupied by X.');
+    });
+
+    it('should not let click outside the game', function () {
+        expect(() => game.tick('X', '0-4')).toThrow('You have ticked outside the board.');
+        expect(() => game.tick('X', '1-4')).toThrow('You have ticked outside the board.');
+        expect(() => game.tick('X', '2-4')).toThrow('You have ticked outside the board.');
+        expect(() => game.tick('X', '3-0')).toThrow('You have ticked outside the board.');
+        expect(() => game.tick('X', '-1-2')).toThrow('You have ticked outside the board.');
+    });
+
+    it('should tell when the game is over and there is a winner', function () {
+        let {result: result1} = game.tick('X', '1-1');
+        expect(result1).toEqual(undefined);
+        let {result: result2} = game.tick('Y', '0-1');
+        expect(result2).toEqual(undefined);
+        let {result: result3} = game.tick('X', '0-0');
+        expect(result3).toEqual(undefined);
+        let {result: result4} = game.tick('Y', '2-0');
+        expect(result4).toEqual(undefined);
+        let {result: result5} = game.tick('X', '2-2');
+        expect(result5).toEqual('X');
+    });
+
 });
---
+++ src/tic-tac-toe/game.ts
@@ -0,0 +1,61 @@
+type Player = 'X' | 'Y';
+
+export class Cell {
+    public occupier?: Player;
+    public coordinates: string;
+    constructor(x: number, y: number) {
+        this.coordinates = `${x}-${y}`;
+    }
+}
+
+export class TicTacToe {
+    private board: Cell[][];
+    private lastToPlay: Player;
+
+    constructor() {
+        const board = [];
+        for (let i = 0; i < 3; i++) {
+            let inner = [];
+            for (let j = 0; j < 3; j++) {
+                inner.push(new Cell(i, j))
+            }
+            board.push(inner)
+        }
+        this.board = board;
+    }
+
+    getBoard(): Cell[][]{
+        return this.board;
+    }
+
+    tick(player: Player, coordinates: string) {
+        if(player === this.lastToPlay) {
+            throw new Error(`It's ${this.getOtherPlayer(player)}'s turn.`)
+        }
+        const [x, y] = coordinates.split('-');
+        let cell = this.board[+x]?.[+y];
+        if(!cell || (!x || !y)) {
+            throw new Error('You have ticked outside the board.');
+        }
+        if(cell.occupier) {
+            throw new Error(`The cell is already occupied by ${this.getOtherPlayer(player)}.`)
+        }
+
+        cell.occupier = player;
+
+        const newBoard: Cell[][] = [];
+        for (let i = 0; i < 3; i++) {
+            const inner = [];
+            for (let j = 0; j < 3; j++) {
+                inner.push({...this.board[i][j]})
+            }
+            newBoard.push(inner);
+        }
+        this.lastToPlay = player;
+        return {newBoard, nextToPlay: this.getOtherPlayer(player)}
+    }
+
+    private getOtherPlayer(player: Player) {
+        return player === 'X' ? 'Y' : 'X';
+    }
+}
